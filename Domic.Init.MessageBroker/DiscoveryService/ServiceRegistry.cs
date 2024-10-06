25a64cf1d23eea0384bd3ad90c46c04a9d60019c
Hasan1375-tech <karami.samdad@gmail.com>
2024-10-06 11:37:32 +0330

    added ticket & financial rabbit configs

---
+++ Domic.Init.MessageBroker/DiscoveryService/ServiceRegistry.cs
@@ -0,0 +1,37 @@
+﻿using Domic.Core.Domain.Constants;
+using Domic.Core.Infrastructure.Extensions;
+using RabbitMQ.Client;
+
+namespace Domic.Init.MessageBroker.DiscoveryService;
+
+public class ServiceRegistry
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="channel"></param>
+    public static void Register(IModel channel)
+    {
+        channel.DirectExchangeDeclare(Broker.ServiceRegistry_Exchange);
+        channel.FanOutExchangeDeclare(Broker.ServiceRegistry_Exchange_Retry_1);
+        channel.FanOutExchangeDeclare(Broker.ServiceRegistry_Exchange_Retry_2);
+    
+        channel.QueueDeclare(Broker.ServiceRegistry_Queue, new Dictionary<string, object> {
+            { "x-delayed-type"         , "fanout" },
+            { "x-dead-letter-exchange" , Broker.ServiceRegistry_Exchange_Retry_1 }
+        });
+    
+        channel.QueueDeclare(Broker.ServiceRegistry_Queue_Retry, new Dictionary<string, object> {
+            { "x-message-ttl"          , 5000 }     , //5s
+            { "x-delayed-type"         , "fanout" } ,
+            { "x-dead-letter-exchange" , Broker.ServiceRegistry_Exchange_Retry_2 }
+        });
+    
+        channel.BindQueueToDirectExchange(Broker.ServiceRegistry_Exchange , Broker.ServiceRegistry_Queue, 
+            Broker.ServiceRegistry_Route
+        );
+    
+        channel.BindQueueToFanOutExchange(Broker.ServiceRegistry_Exchange_Retry_1 , Broker.ServiceRegistry_Queue_Retry);
+        channel.BindQueueToFanOutExchange(Broker.ServiceRegistry_Exchange_Retry_2 , Broker.ServiceRegistry_Queue);
+    }
+}
--- Domic.Init.MessageBroker/Program.cs
+++ Domic.Init.MessageBroker/Program.cs
@@ -1,6 +1,5 @@
-﻿using Domic.Core.Domain.Constants;
-using Domic.Core.Infrastructure.Extensions;
-using Domic.Init.MessageBroker.AggregateArticleService;
+﻿using Domic.Init.MessageBroker.AggregateArticleService;
+using Domic.Init.MessageBroker.DiscoveryService;
 using Domic.Init.MessageBroker.FinancialService;
 using Domic.Init.MessageBroker.StateTrackerService;
 using Domic.Init.MessageBroker.TermService;
@@ -123,28 +122,7 @@ try
     UserOfAggregateFinancial.Register(channel);
     
     //ServiceRegistry
-    
-    channel.DirectExchangeDeclare(Broker.ServiceRegistry_Exchange);
-    channel.FanOutExchangeDeclare(Broker.ServiceRegistry_Exchange_Retry_1);
-    channel.FanOutExchangeDeclare(Broker.ServiceRegistry_Exchange_Retry_2);
-    
-    channel.QueueDeclare(Broker.ServiceRegistry_Queue, new Dictionary<string, object> {
-        { "x-delayed-type"         , "fanout" },
-        { "x-dead-letter-exchange" , Broker.ServiceRegistry_Exchange_Retry_1 }
-    });
-    
-    channel.QueueDeclare(Broker.ServiceRegistry_Queue_Retry, new Dictionary<string, object> {
-        { "x-message-ttl"          , 5000 }     , //5s
-        { "x-delayed-type"         , "fanout" } ,
-        { "x-dead-letter-exchange" , Broker.ServiceRegistry_Exchange_Retry_2 }
-    });
-    
-    channel.BindQueueToDirectExchange(Broker.ServiceRegistry_Exchange , Broker.ServiceRegistry_Queue, 
-        Broker.ServiceRegistry_Route
-    );
-    
-    channel.BindQueueToFanOutExchange(Broker.ServiceRegistry_Exchange_Retry_1 , Broker.ServiceRegistry_Queue_Retry);
-    channel.BindQueueToFanOutExchange(Broker.ServiceRegistry_Exchange_Retry_2 , Broker.ServiceRegistry_Queue);
+    ServiceRegistry.Register(channel);
     
 }
 catch(Exception e)
